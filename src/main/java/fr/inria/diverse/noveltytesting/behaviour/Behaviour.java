f7d536e47c74fc471b184e842e239d140d019914
Mohamed Boussaa <mohamed.boussaa@inria.fr>
2014-10-24 14:01:13 +0200

    Update Behaviour

--- src/main/java/fr/inria/diverse/noveltytesting/behaviour/Behaviour.java
+++ src/main/java/fr/inria/diverse/noveltytesting/behaviour/Behaviour.java
@@ -1,21 +1,17 @@
 package fr.inria.diverse.noveltytesting.behaviour;
 
+import fr.inria.diverse.noveltytesting.model.Interface;
+import fr.inria.diverse.noveltytesting.model.Population;
+
 /**
  * Created by leiko on 16/10/14.
  */
+
 public interface Behaviour {
 
-    /**
-     * Determine the distance, in behaviour space, between this Behaviour and the given Behaviour.
-     * All distances should be in the range [0, 1].
-     * @param b behaviour
-     * @return double
-     */
-    double distanceFrom(Behaviour b);
+	
+	void setNoveltyMetric(Interface anInterface,Population population,Population Archive);
+	double getNoveltyMetric();
 
-    /**
-     * Provide a default/suggested threshold. Should be in the range [0, 1].
-     * @return double
-     */
-    double defaultThreshold();
+
 }
