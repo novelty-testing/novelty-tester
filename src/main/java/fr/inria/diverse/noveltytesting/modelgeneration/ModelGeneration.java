bed069e08ad159f55e3b66aa5435f2972fefcd65
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-24 11:23:12 +0200

    allow ModelGeneration to change its Generator

--- src/main/java/fr/inria/diverse/noveltytesting/modelgeneration/ModelGeneration.java
+++ src/main/java/fr/inria/diverse/noveltytesting/modelgeneration/ModelGeneration.java
@@ -1,5 +1,6 @@
 package fr.inria.diverse.noveltytesting.modelgeneration;
 
+import fr.inria.diverse.noveltytesting.generator.Generator;
 import fr.inria.diverse.noveltytesting.model.Interface;
 
 import java.lang.reflect.InvocationTargetException;
@@ -8,6 +9,8 @@ import java.lang.reflect.InvocationTargetException;
  * Created by leiko on 16/10/14.
  */
 public interface ModelGeneration {
+
+    void setGenerator(Generator gen);
 
     Interface generateModel(Class<?> clazz);
 
