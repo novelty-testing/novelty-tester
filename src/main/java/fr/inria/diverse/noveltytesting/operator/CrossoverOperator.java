2379d45f83db38448b655b3c6247c90f53713239
Mohamed Boussaa <mohamed.boussaa@inria.fr>
2014-10-27 12:03:24 +0100

    first update crossover

--- src/main/java/fr/inria/diverse/noveltytesting/operator/CrossoverOperator.java
+++ src/main/java/fr/inria/diverse/noveltytesting/operator/CrossoverOperator.java
@@ -10,46 +10,41 @@ import java.util.List;
  */
 public class CrossoverOperator implements Operator {
 
-    @Override
-    public void process(Population population) {
-        List<Interface> interfaces = population.getInterfaces();
+	@Override
+	public void process(Population population) {
+		int crossoverPoint = 0;
+		int crossoverMethodPosition = 0;
+		Object permute = null;
 
-        // TODO
-        // Do not use this code below, rewrite it according to the new Population model etc
+		List<Interface> interfaces = population.getInterfaces();
+		if (interfaces.size() > 1) {
+			for (int i = 0; i < interfaces.size() - 1; i++) {
 
-//        int crossoverPoint = 0;
-//        int crossoverMethodPosition = 0;
-//        Object permute=null;
-//
-//        if (selectedTestSequenceSize>1) {
-//
-//            for (int i = 0; i < selectedTestSequenceSize-1 ;i++) {
-//
-//                //crossoverPoint= 0;
-//                crossoverMethodPosition= (int) (Math.random() * selectedTestSequence.elementAt(i).tc.size());
-//                crossoverPoint= (int) (Math.random() * selectedTestSequence.elementAt(i).tc.elementAt(crossoverMethodPosition).data.length);
-//
-//
-//                permute=selectedTestSequence.elementAt(i).tc.elementAt(crossoverMethodPosition).data[crossoverPoint];
-//                selectedTestSequence.elementAt(i).tc.elementAt(crossoverMethodPosition).data[crossoverPoint]=selectedTestSequence.elementAt(i+1).tc.elementAt(crossoverMethodPosition).data[crossoverPoint];
-//                selectedTestSequence.elementAt(i+1).tc.elementAt(crossoverMethodPosition).data[crossoverPoint]=permute;
-//
-//
-//                //System.out.println(selectedTestSequence.elementAt(i).tc.elementAt(crossoverMethodPosition).m.getName());
-//                //System.out.println(crossoverMethodPosition);
-//                //System.out.println(crossoverPoint);
-//                //System.out.println(selectedTestSequence.elementAt(i).tc.elementAt(crossoverMethodPosition).data[crossoverPoint]);
-//                //System.out.println(selectedTestSequence.elementAt(i+1).tc.elementAt(crossoverMethodPosition).data[crossoverPoint]);
-//                //System.out.println();
-//                //Object[] data=new RandomData().getMutationDataGenerated(selectedTestSequence.elementAt(i).tc.get(j).m,selectedTestSequence.elementAt(i).tc.get(j).data);
-//                //selectedTestSequence.elementAt(i).tc.get(j).setData(data);
-//
-//                //TestCase tc=new TestCase (selectedTestSequence.elementAt(i).tc.get(j).m,data,null);
-//
-//                //testSuite.addTestCase(tc);
-//
-//            }
-//            //newTestSequence.add(testSuite);
-//        }
-    }
+				crossoverMethodPosition = (int) (Math.random() * interfaces
+						.get(i).getMethods().size());
+				crossoverPoint = (int) (Math.random() * interfaces.get(i)
+						.getMethods().get(crossoverMethodPosition)
+						.getParameters().size());
+
+				permute = interfaces.get(i).getMethods()
+						.get(crossoverMethodPosition).getParameters()
+						.get(crossoverPoint).getValue();
+				interfaces
+						.get(i)
+						.getMethods()
+						.get(crossoverMethodPosition)
+						.getParameters()
+						.get(crossoverPoint)
+						.setValue(
+								interfaces.get(i + 1).getMethods()
+										.get(crossoverMethodPosition)
+										.getParameters().get(crossoverPoint)
+										.getValue());
+				interfaces.get(i + 1).getMethods().get(crossoverMethodPosition)
+						.getParameters().get(crossoverPoint).setValue(permute);
+			}
+
+		}
+
+	}
 }
